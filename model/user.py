4fded04a6b958494d301ecf0d589e2b74cb3ea15
Angel Suarez <angelsuarez@Angels-MacBook-Pro.local>
2024-06-10 11:36:36 -0400

    Added user DAO and uniqueness of email

--- model/user.py
+++ model/user.py
@@ -8,6 +8,36 @@ class User:
 
     def validate_unique_email(self, email, user_dao):
         existing_user = user_dao.get_user_by_email(email)
-        if existing_user:
-            return False
-        return True
+        return existing_user is None
+
+class UserDAO:
+    def __init__(self):
+        # User storage
+        self.users = []
+
+    def get_user_by_email(self, email):
+        # Database lookup
+        for user in self.users:
+            if user.email == email:
+                return user
+        return None
+
+    def add_user(self, user):
+        self.users.appen(user)
+
+# Creates an instance of UserDAO
+user_dao = UserDAO()
+
+# Create a new User instance
+new_user = User(user_id = 1, email = "", name = "")
+
+# Add the new user to the user_dao (for testing purposes)
+user_dao.add_user(new_user)
+
+#Validate if the email is unique
+is_unique = new_user.validate_unique_email("", user_dao)
+
+if is_unique:
+    print("Email is available.")
+else:
+    print("Email already in use. Please use a different one.")
