a81d6f3f049ac43343d1744cb93d3e31621ef3d4
niniisg <156111441+niniisg@users.noreply.github.com>
2024-06-10 15:09:43 -0400

    Delete model/review.py

--- model/review.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-class Review:
-    def __init__(self, review_id, place, user, rating, comment):
-        self.review_id = review_id
-        self.place = place
-        self.user = user
-        self.rating = rating
-        self.comment = comment
